70cd3f85d0fc49c199d149bb5fc5e8fca4f1c40b
carotkut94 <carotkut12@gmail.com>
2020-03-17 10:01:17 +0530

    Initial Commit

---
+++ NeuMorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeuMorphism
+//
+//  Created by Death Code on 17/03/20.
+//  Copyright © 2020 Death Code. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
